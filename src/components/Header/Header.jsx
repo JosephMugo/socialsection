68aa5c65af7b930e1c52248df7725e9b3d0c8c2a
JosephMugo <joemu18@gmail.com>
2020-09-15 00:08:36 -0500

    added components and there styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,4 @@
+//                 HEADER COMPONENT
+//     10,000+ of our users love our products.
+//     We only provide great products combined with excellent customer service.
+//     See what our satisfied customers are saying about our services.
---
+++ src/components/Ratings/RatingsList/RatingsList.jsx
@@ -0,0 +1,5 @@
+//                 RATINGS COMPONENT
+//                      ----> RATING COMPONENT
+//     Rated 5 Stars in Reviews
+//     Rated 5 Stars in Report Guru
+//     Rated 5 Stars in BestTech
