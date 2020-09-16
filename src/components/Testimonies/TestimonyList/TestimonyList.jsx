331ff392f7d22cca5144d936349b9e533178438d
JosephMugo <joemu18@gmail.com>
2020-09-16 15:53:34 -0500

    updated testimonies to include different users;

--- src/components/Testimonies/TestimonyList/TestimonyList.jsx
+++ src/components/Testimonies/TestimonyList/TestimonyList.jsx
@@ -16,23 +16,38 @@
 //     "Put an order with this company and can only praise them for the very high
 //     standard. Will definitely use them again and recommend them to everyone!"
 import React from 'react';
-import userImg from '../../../images/image-colton.jpg'
+import coltonImg from '../../../images/image-colton.jpg';
+import ireneImg from '../../../images/image-irene.jpg';
+import anneImg from '../../../images/image-anne.jpg';
 import Testimony from '../Testimony/Testimony';
 import './TestimonyList.css';
 
-const user = {
-    image: userImg,
-    name: "Colton Smith",
-    type: "Verified Buyer",
-    description: "We needed the same printed design as the one we had ordered a week prior.Not only did they find the original order, but we also received it in time.Excellent!"
+const Colton = {
+    image: coltonImg,
+    name: 'Colton Smith',
+    type: 'Verified Buyer',
+    description: 'We needed the same printed design as the one we had ordered a week prior.Not only did they find the original order, but we also received it in time.Excellent!'
 }
 
+const Irene = {
+    image: ireneImg,
+    name: 'Irene Roberts',
+    type: 'Verified Buyer',
+    description: 'Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.'
+}
+
+const Anne = {
+    image: anneImg,
+    name: 'Anne Wallace',
+    type: 'Verified Buyer',
+    description: 'Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!'
+}
 const TestimonyList = () => {
     return (
         <div className="testimonyListContainer">
-            <Testimony key='1' user={user} />
-            <Testimony key='2' user={user} />
-            <Testimony key='3' user={user} />
+            <Testimony key='1' user={Colton} />
+            <Testimony key='2' user={Irene} />
+            <Testimony key='3' user={Anne} />
         </div>
     )
 }
